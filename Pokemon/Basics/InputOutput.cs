00afe173caf5e4e216cfbfa20dc454f0f7030562
Prateek <prateek.sharma@humanpoweredhealth.com>
2026-06-20 18:46:19 +0530

    Basic C# Class Added

---
+++ Pokemon/Basics/InputOutput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Basics
+{
+    internal class InputOutput
+    {
+        public static void Run() {
+
+            Console.WriteLine("Hey, Pokemon this side !!!");
+            int age = 10;
+            string Name = "Pikachu";
+            Console.WriteLine("My name is " + Name + "and my age is " + age);
+
+
+            //------------------------------------
+            // Building the Name Questioning Model
+
+            Console.Write("Please Enter Your name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello" + userName);
+     
+        }
+    }
+}
--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -1,20 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Pokemon.Basics;
+
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hey, Pokemon this side !!!");
 
-int age = 10;
-string Name = "Pikachu";
-
-Console.WriteLine("My name is " + Name + "and my age is " + age);
-
-
-//-------------------------------------------------------------------------------------------
-// Building the Name Questioning Model
-
-Console.Write("Please Enter Your name: ");
-string userName = Console.ReadLine();
-
-Console.WriteLine("Hello" + userName);
-//-------------------------------------------------------------------------------------------
-
+// Because member is static, So no object is required !!
+InputOutput.Run();
 
