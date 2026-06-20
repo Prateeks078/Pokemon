630c476233a84b8a344d3d6d9089f6ff5a51667f
Prateeks078 <itsprateek078@gmail.com>
2026-06-20 22:39:02 +0530

    Updating the Array Module

---
+++ Pokemon/Basics/Arrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Basics
+{
+    internal class Arrays
+    {
+        public static void Run(){
+            // Declaring an Array
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            //Declaring a character array of a fixed length 
+            string[] arr2 = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Pokemon.Basics;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 // Because member is static, So no object is required !!
-InputOutput.Run();
+//InputOutput.Run();
 
+Arrays.Run();
