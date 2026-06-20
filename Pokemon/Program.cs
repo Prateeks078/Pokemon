0bfe157c2ec16945e2636a6dd8f1a66d4fb1c35a
Prateek <prateek.sharma@humanpoweredhealth.com>
2026-06-20 12:29:21 +0530

    Add project files.

---
+++ Pokemon/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hey, Pokemon this side !!!");
+
+int age = 10;
+string Name = "Pikachu";
+
+Console.WriteLine("My name is " + Name + "and my age is " + age);
+
+
+//-------------------------------------------------------------------------------------------
+// Building the Name Questioning Model
+
+Console.Write("Please Enter Your name: ");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + userName);
+//-------------------------------------------------------------------------------------------
+
+
