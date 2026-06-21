ca41bf7783f57f156f495bab6b6c405b2058d8c1
Prateeks078 <itsprateek078@gmail.com>
2026-06-21 10:54:22 +0530

    Working with Getters & Setters

---
+++ Pokemon/Basics/GettersSetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Basics
+{
+    internal class Movie
+    {
+        public string name;
+        private int rating;// Making it private for not disturbing it outside this class..
+        public string actor;
+
+        // Writing a Constructor
+        public Movie(string mname,int mrating,string mactor) {
+            name = mname;
+            Rating = mrating;
+            actor = mactor;
+        }
+
+        //  Getter-Setters
+        public int Rating {
+            get  { return rating; }
+            set { if (value >= 0) rating=value; }
+        }
+    }
+}
--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -24,14 +24,25 @@ using Pokemon.Basics;
 
 
 
-
 //----- Starting with the Constructors (Constructor Class) -----
 Car car1 = new Car("Mustang", 340000, "Elon Musk");
 //Console.WriteLine(car1.brand);
+
+
 
 // ----- Starting with the ObjectMethods Concepts (ObjectMethods.cs Class) ----
 Student s1 = new Student("Anish", "Mathematics", 3.4);
 Student s2 = new Student("Alex", "Mathematics", 9.5);
 
-Console.WriteLine(s1.hasMerit());
-Console.WriteLine(s2.hasMerit());
+//Console.WriteLine(s1.hasMerit());
+//Console.WriteLine(s2.hasMerit());
+
+
+
+// ---- Working with the Getters & Setters(Putting a Double Wrapper Security Guard Encapsulation) -------
+Movie m1 = new Movie("Harry-Potter", 9, "Mark James");
+m1.Rating = -1; // This Line will never execute in this case 
+
+
+Movie m2 = new Movie("Harry", -9999, "Mark J");
+Console.WriteLine(m2.Rating); // In this case -999 will never be going to set coz if(value>=0) will give us false, hence default value 0 will come to o/p
